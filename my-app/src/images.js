9e3df976204a6d775f4e00d456b8c4f5684730df
Isabela Rabasa <isabelarabasa@Isabelas-MacBook-Pro.local>
2019-07-26 16:25:18 -0500

    first

---
+++ my-app/src/images.js
@@ -0,0 +1,29 @@
+import door1 from './images/door1.png';
+import door2 from './images/door2.png';
+import door3 from './images/door3.png';
+import door4 from './images/door4.png';
+import door5 from './images/door5.png';
+import door6 from './images/door6.png';
+import door7 from './images/door7.png';
+import door8 from './images/door8.png';
+import door9 from './images/door9.png';
+import door10 from './images/door10.png';
+import door11 from './images/door11.png';
+import door12 from './images/door12.png';
+
+const images = [
+    door1,
+    door2,
+    door3,
+    door4,
+    door5,
+    door6,
+    door7,
+    door8,
+    door9,
+    door10,
+    door11,
+    door12
+];
+
+export default images;
